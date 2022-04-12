824f741e8bbe8231e98aef8295e201675e7ce5d0
Vlad Klinov <klinowlad@gmail.com>
2022-04-12 14:41:34 +0300

    123

--- app/src/main/java/com/klinovvlad/task3klinov/model/DataTest.kt
+++ app/src/main/java/com/klinovvlad/task3klinov/model/DataTest.kt
@@ -7,7 +7,7 @@ data class DataTest(@SerializedName("results") val results: List<DataResult>)
 data class DataResult(@SerializedName("name") val name: DataName)
 
 data class DataName(
-    @SerializedName("title") val title: String,
-    @SerializedName("first") val first: String,
-    @SerializedName("last") val last: String
+    val title: String,
+    val first: String,
+    val last: String
 )
