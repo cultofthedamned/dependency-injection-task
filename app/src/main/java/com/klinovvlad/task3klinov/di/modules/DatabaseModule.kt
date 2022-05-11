0a5b84872c48e18ff385d6391f8aa9ededb09b9b
Vlad Klinov <klinowlad@gmail.com>
2022-05-11 14:24:33 +0300

    dagger

--- app/src/main/java/com/klinovvlad/task3klinov/di/modules/DatabaseModule.kt
+++ app/src/main/java/com/klinovvlad/task3klinov/di/modules/DatabaseModule.kt
@@ -3,7 +3,6 @@ package com.klinovvlad.task3klinov.di.modules
 import android.content.Context
 import com.klinovvlad.task3klinov.db.UserDao
 import com.klinovvlad.task3klinov.db.UserDatabase
-import com.klinovvlad.task3klinov.model.UserDatabaseRepository
 import dagger.Module
 import dagger.Provides
 
--- app/src/main/java/com/klinovvlad/task3klinov/di/modules/NetworkModule.kt
+++ app/src/main/java/com/klinovvlad/task3klinov/di/modules/NetworkModule.kt
@@ -1,6 +1,5 @@
 package com.klinovvlad.task3klinov.di.modules
 
-import com.klinovvlad.task3klinov.model.UserNetworkRepository
 import com.klinovvlad.task3klinov.network.api.UserApi
 import dagger.Module
 import dagger.Provides
